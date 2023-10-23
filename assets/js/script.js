1ba155f0e6f79baa8b077215916dcded81227505
duweri <danielleuweri01@gmail.com>
2023-10-23 14:41:17 +0100

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+ /* GIVEN I am taking a code quiz
+ 
+WHEN I click the start button
+  // Taken to the next page 
+  //Timer starts from 1 minute 
+  // Presented with first question and multiple choice
+
+THEN a timer starts and I am presented with a question
+
+
+WHEN I answer a question
+  //When right answer is clicked, next question is presented
+
+THEN I am presented with another question
+
+
+WHEN I answer a question incorrectly
+THEN time is subtracted from the clock
+
+
+WHEN all questions are answered or the timer reaches 0
+  // pop up saying "you beat the time!"
+  // If not all questions answered then time stops and pop up saying "you lose!"
+
+THEN the game is over
+WHEN the game is over
+THEN I can save my initials and score */
+
