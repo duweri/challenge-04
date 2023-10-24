e7a1ce5dd8fa950eaae3cf2452bb65144942730b
duweri <danielleuweri01@gmail.com>
2023-10-24 16:13:04 +0100

    third commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,53 @@
 var startButton = document.querySelector(".start");
-var questionOne = [
+var questions = [
 	{
 		question: "What is a function?",
 		answers: {
 			a: "stores a series of characters",
 			b: "block of text that carries out a function",
 			c: "block of code that carries out a task",
+            
 		},
-		correctAnswer: 'c'
-	}]
-
-    var questionTwo = [
-        {
-            question: "What does CSS stand for?",
+        correctAnswer: 'c' },
+		
+	
+        { question: "What does CSS stand for?",
             answers: {
                 a: "Central style sheet",
                 b: "Cascading style sheets",
                 c: "Cascading simple sheets",
             },
-            correctAnswer: 'b'
-        }]
+            correctAnswer: 'b' },
+        
     
-    var questionThree = [
+           { question: "What is a function?",
+            answers: {
+                a: "stores a series of characters",
+                b: "block of text that carries out a function",
+                c: "block of code that carries out a task",
+            },
+            correctAnswer: 'c'},
+        
+
+   
+           { question: "What is a function?",
+            answers: {
+                a: "stores a series of characters",
+                b: "block of text that carries out a function",
+                c: "block of code that carries out a task",
+            },
+            correctAnswer: 'c'},
+        
+
+           { question: "What is a function?",
+            answers: {
+                a: "stores a series of characters",
+                b: "block of text that carries out a function",
+                c: "block of code that carries out a task",
+            },
+            correctAnswer: 'c'},
+        
+   
         {
             question: "What is a function?",
             answers: {
@@ -29,21 +55,48 @@ var questionOne = [
                 b: "block of text that carries out a function",
                 c: "block of code that carries out a task",
             },
-            correctAnswer: 'c'
-        },]
+            correctAnswer: 'c'},
+        
+        {
+            question: "What is a function?",
+            answers: {
+                a: "stores a series of characters",
+                b: "block of text that carries out a function",
+                c: "block of code that carries out a task",
+            },
+            correctAnswer: 'c'},
+        ]
+
+
+var secondsLeft = 60;
+var timeEL = document.getElementById("timer");
+
 
  /*WHEN I click the start button*/
  // Taken to the next page 
 startButton.addEventListener("click", startGame);
 function startGame() {
    document.querySelector(".start").style.display = "none"
+//Timer starts from 1 minute 
+   function setTime() {
+    console.log("hey");
+    var timerInterval = setInterval(function() {
+        secondsLeft--;
+        timeEL.textContent = secondsLeft + " seconds"
+
+        if(secondsLeft === 0) {
+            clearInterval(timerInterval);
+        }
+
+    }, 1000);
+
+} 
+setTime();  
 }
 
-
-    
-    
-
- 
+/*for(var i=0; i < questions.length; i++ ) {
+    var response = 
+}
  
  
  
